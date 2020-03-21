6a988b23257ca6d51a2b97943cbcd34ccc17b57e
iyasmzn <iyasmzn07@gmail.com>
2020-03-21 08:56:50 +0700

    completed 5th file

--- file5.php
+++ file5.php
@@ -13,9 +13,34 @@ class Player {
 }
 
 class Score extends Player {
-	public $score;
+	private $score;
 	public function set_score($sc) {
-
+		$this->score = $sc;
+	}
+	public function msg_score() {
+		echo "$this->nama made $this->score goals this season and he was {$this->umur}th years old.";
 	}
 }
+
+class Assist extends Player {
+	private $assist;
+	public function set_assist($ass) {
+		$this->assist = $ass;
+	}
+	public function msg_assist() {
+		echo "$this->nama made $this->assist assists this season and he was {$this->umur}th years old.";
+	}
+}
+
+$gol = new Score('Ronaldo');
+$gol->set_age(37);
+$gol->set_score(365);
+$gol->msg_score();
+
+echo "<br>";
+
+$ass = new Assist('Kroos');
+$ass->set_age(28);
+$ass->set_assist(200);
+$ass->msg_assist();
 ?>
